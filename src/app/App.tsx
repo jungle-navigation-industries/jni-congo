e4d4e21b65ddd3126ea33495e5802c602e5ee414
Michael Bradvica <mjbradvica@gmail.com>
2023-03-01 16:06:01 -0500

    can hit esi api

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,12 @@
 const App: React.FC = () => {
-  return <h1>Jungle Navigation Industries</h1>;
+  return (
+    <main>
+      <h1>Jungle Navigation Industries</h1>
+      <a href="https://login.eveonline.com/oauth/authorize?response_type=code&redirect_uri=https://localhost/callback/&client_id=dd02558f01f84d38848349a7ebe4f1f0&scope=publicData">
+        <button>Log In</button>
+      </a>
+    </main>
+  );
 };
 
 export default App;
